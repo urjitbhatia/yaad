b8a80d6e985f0c4caef2e5c5ea41bc15e932ce64
Urjit Singh Bhatia <urjitsb87@gmail.com>
2018-10-01 07:03:35 +0000

    Add job and spoke implementation of yaad in go

---
+++ pkg/yaad/yaad_suite_test.go
@@ -0,0 +1,13 @@
+package yaad_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestYaad(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Yaad Suite")
+}
