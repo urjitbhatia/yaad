b13493be5e9311d862f192c3e38e38d1b9857f80
Urjit Singh Bhatia <urjitsb87@gmail.com>
2018-10-20 21:23:10 +0000

    Silly C++ mistake, body slices are already refs

--- pkg/protocol/yaadsrv.go
+++ pkg/protocol/yaadsrv.go
@@ -61,7 +61,7 @@ func (t *TubeYaad) pauseTube(delay time.Duration) error {
 }
 
 func (t *TubeYaad) put(delay int, pri int32, body []byte, ttr int) (string, error) {
-	j := yaad.NewJobAutoID(time.Now().Add(time.Second*time.Duration(delay)), &body)
+	j := yaad.NewJobAutoID(time.Now().Add(time.Second*time.Duration(delay)), body)
 	j.SetOpts(pri, time.Duration(ttr)*time.Second)
 
 	t.hub.AddJob(j)
@@ -79,9 +79,9 @@ func (t *TubeYaad) reserve(timeoutSec string) *Job {
 	// try once
 	if j := t.hub.Next(); j != nil {
 		return &Job{
-			body: *j.Body(),
+			body: j.Body(),
 			id:   j.ID(),
-			size: len(*j.Body()),
+			size: len(j.Body()),
 		}
 	}
 
@@ -92,9 +92,9 @@ func (t *TubeYaad) reserve(timeoutSec string) *Job {
 	for start.Add(time.Duration(ts) * time.Second).After(time.Now()) {
 		if j := t.hub.Next(); j != nil {
 			return &Job{
-				body: *j.Body(),
+				body: j.Body(),
 				id:   j.ID(),
-				size: len(*j.Body()),
+				size: len(j.Body()),
 			}
 		}
 		time.Sleep(time.Millisecond * 200)
